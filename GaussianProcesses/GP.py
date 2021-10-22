9c94efae8e02575ca0f00f031b6e8918ee85fd5c
inquizition <47603371+inquizition@users.noreply.github.com>
2021-10-22 20:44:47 +0200

    Made changes to algorithm, GP

--- GaussianProcesses/GP.py
+++ GaussianProcesses/GP.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def K(gp, x1, x2):
+        return kernel(x1, x2, gp.l, gp.sigma)
+
+def kernel(x, mu, l, sigma):
+        sqdist = np.sum(x**2, 1).reshape(-1,1) + np.sum(mu**2, 1) - 2 * np.dot(x, mu.T)    
+        return sigma**2 * np.exp(-0.5 / l**2 * sqdist)
+
 class GP:
 
     def __init__(self, sigma, l):
@@ -7,17 +14,10 @@ class GP:
         self.l = l
         self.kern = 'gp'
 
-    def kernel(self, x, mu):
-        sqdist = np.sum(x**2, 1).reshape(-1,1) + np.sum(mu**2, 1) - 2 * np.dot(x, mu.T)    
-        return self.sigma**2 * np.exp(-0.5 / self.l**2 * sqdist)
-
-    def K(self, x1, x2):
-        return self.kernel(x1, x2)
-
     def posterior(self, x, t, x_test, var):
-        Sn_inv = (var)*np.eye(len(x)) + self.K(x,x)
-        m_N = (self.K(x, x_test).T).dot(np.linalg.pinv(Sn_inv))
-        S_N = (self.K(x_test,x_test) + var*np.eye(len(x_test)) - (m_N.dot(self.K(x,x_test))))
+        Sn_inv = (var)*np.eye(len(x)) + K(self, x,x)
+        m_N = (K(self, x, x_test).T).dot(np.linalg.pinv(Sn_inv))
+        S_N = (K(self, x_test,x_test) + var*np.eye(len(x_test)) - (m_N.dot(K(self, x,x_test))))
         m_N = m_N.dot(t)
 
         return m_N, S_N
