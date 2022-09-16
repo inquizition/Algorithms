27730e7c477edca29e93c3712ffebdcb5b45f05e
Linus Ryttermalm <linus.ryttermalm@prevas.se>
2022-09-16 17:28:55 +0200

    Added c code trueskill

--- Bayesian/Trueskill/Message.c
+++ Bayesian/Trueskill/Message.c
@@ -3,7 +3,11 @@
 #include "Message.h"
 
 
-
+void update_skills_MP(struct Message t1,struct Message t2, float var_t, float y_obs)
+{
+    struct Message mu1 = { .mu = t1.mu, .var = t1.var};
+    struct Message mu1 = { .mu = t2.mu, .var = t2.var};
+}
 static struct Message multiply(struct Message m_1, struct Message m_2)
 {
     struct Message m_out;
@@ -20,31 +24,74 @@ static struct Message divide(struct Message m_1, struct Message m_2)
     return m_out;
 }
 
-static struct Message truncMM(struct Message m, uint32_t y_obs)
+static struct Message truncMM(struct Message m, float y_obs)
 {
+    struct Message m_out;
     if(y_obs == 1)
     {
-
+        float a = 0.0;
+        float b = 100;
+        m_out.mu = MM_mean(a,b,m.mu,m.var);
+        m_out.var = MM_var(a,b,m.mu,m.var);
     }
     else if(y_obs == 0)
     {
-
+        float a = 100;
+        float b = 0.0;
+        m_out.mu = MM_mean(a,b,m.mu,m.var);
+        m_out.var = MM_var(a,b,m.mu,m.var);
     }
     else
     {
-
+        print("error!");
     }
-    struct Message m_out;
+    return m_out;
 
 }
 
-static uint32_t MM_mean(uint8_t a, uint8_t alpha, uint32_t mu, uint32_t Z) 
+static float stdNormalDist(Z)
 {
-
-    return erfc(3);
+    return exp(((pow(-Z,2))/2))/(sqrt(2*3.1415));
 }
 
-static uint32_t MM_var()
+static float MM_mean(float a, float b, float mu, float var) 
 {
+    if(a == 0.0)
+    {
+        float alpha = (a-mu)/var;
+        float Z = 1 - ((1/2)*(2 - erfc(alpha/(sqrt(2)))));
+        return mu + var*stdNormalDist(alpha)/Z;
+    }
+    else if(b == 0.0)
+    {
+        float beta = (b - mu)/var;
+        float Z = ((1/2)*(2 - erfc(beta/(sqrt(2)))));
+        return mu - var*(stdNormalDist(beta)/Z);
+    }
+    else
+    {
+        print("neither 0.0");
+        return -1;
+    }
+    
+}
 
+static float MM_var(float a, float b, float mu, float var)
+{
+    if(a == 0)
+    {
+        float alpha = (a - mu)/var;
+        float Z = 1 - ((1/2)*(2 - erfc(alpha/(sqrt(2)))));
+        return pow(var,2) * (1 + ((alpha*(stdNormalDist(alpha))/Z)) - pow((stdNormalDist(alpha)/Z),2));
+    }
+    else if(b == 0)
+    {
+        float beta = (b - mu)/var;
+        float Z = ((1/2)*(2 - erfc(beta/(sqrt(2)))));
+        return pow(var,2) * (1 - (beta*(stdNormalDist(beta)/Z)) - pow((stdNormalDist(beta)/Z),2));
+    }
+    else
+    {
+        return -1;
+    }
 }
