d2276f1f42fde59698c65cac1dedaf18ed51a9ea
inquizition <47603371+inquizition@users.noreply.github.com>
2022-09-18 00:47:11 +0200

    Added userinput

--- Bayesian/Trueskill/Trueksill_C_imp/Trueskill.c
+++ Bayesian/Trueskill/Trueksill_C_imp/Trueskill.c
@@ -3,20 +3,31 @@
 
 void main( void )
 {
+    char buffer[10];
     struct Message team1 = { .mu = 1, .var = 1};
     struct Message team2 = { .mu = 1, .var = 1};
 
-    float y_obs[5] = {1.0, 0.0, 1.0, 0.0, 1.0};
+    float y_obs;
 
-    float start_var = 5; // 
+    float start_var; // 
 
-    uint8_t i;
-    for (i = 0; i < 5; i++)
+    printf("Enter mean and variance of first team: ");
+    scanf("%f %f", &team1.mu, &team1.var);
+    printf("\n");
+    printf("Enter mean and variance of second team: ");
+    scanf("%f %f", &team2.mu, &team2.var);
+    printf("\n");
+    printf("Enter start variance of match: ");
+    scanf("%f", &start_var);
+    printf("\n");
+
+
+    while(1)
     {
-        start_var = update_skills_MP(&team1, &team2, start_var, y_obs[i]);
-  
-
-        printf("\nMean team 1: %f, var: %f\n", team1.mu, team1.var);
+        printf("\nWho won?\n");
+        scanf("%f", &y_obs);
+        start_var = update_skills_MP(&team1, &team2, start_var, y_obs);
+        printf("Mean team 1: %f, var: %f\n", team1.mu, team1.var);
         printf("Mean team 2: %f, var: %f\n",team2.mu, team2.var);
     }
 }
