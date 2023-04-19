4fcf25ce90c29a985ec2d15626e902eef9bc4fb9
Linus Ryttermalm <linus.ryttermalm@prevas.se>
2023-04-19 17:44:45 +0200

    main and linear model update

--- Bayesian/VAE/Models/LinearModel.c
+++ Bayesian/VAE/Models/LinearModel.c
@@ -1,9 +1,14 @@
 #include "Matrix.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 typedef struct
 {
 	Matrix *A;
 	Matrix *b;
+	bool output_init;
+	Matrix *output;
 }LM;
 
 LM *InitLinear(int a, int b)
@@ -14,7 +19,15 @@ LM *InitLinear(int a, int b)
 	model_ptr->A = allocateMatrix(a, b);
 	InitRandomMatrix(model_ptr->A);
 	model_ptr->b = allocateMatrix(b,1);
-	InitRandomMatrix(model_ptr->b);	
+	InitRandomMatrix(model_ptr->b);
+	model_ptr->output_init = false;
+
+	return model_ptr; 
+}
+
+void forward(LM* linear)
+{
+
 }
 
 void freeLinear(LM *linear)
--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 #include "LinearModel.h"
 
 
-void main(void)
+int main(void)
 {
+	printf("Start application.");
 
+	LM *linear = InitLinear(4,4);
+
+	print_matrix(*linear->A);
+	print_matrix(*linear->b);
+
+	freeLinear(linear);
+	return 1;
 }
