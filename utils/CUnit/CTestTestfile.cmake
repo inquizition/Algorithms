380c7e87ee07875723f13d572bb4d0dbe0890970
linus <ryttermalm@gmail.com>
2023-05-01 21:24:54 +0200

    remove cunit, fix lib

--- utils/CUnit/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linus/Documents/Git_repos/Algorithms/utils/cunit/CUnit
-# Build directory: /home/linus/Documents/Git_repos/Algorithms/utils/cunit/local-build/CUnit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cunit_test "/home/linus/Documents/Git_repos/Algorithms/utils/cunit/local-build/CUnit/cunit_test")
-set_tests_properties(cunit_test PROPERTIES  _BACKTRACE_TRIPLES "/home/linus/Documents/Git_repos/Algorithms/utils/cunit/CUnit/CMakeLists.txt;88;add_test;/home/linus/Documents/Git_repos/Algorithms/utils/cunit/CUnit/CMakeLists.txt;0;")
