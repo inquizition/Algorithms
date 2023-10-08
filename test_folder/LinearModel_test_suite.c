43f17e04621251706e99bfda610019140f5ce51d
linus <ryttermalm@gmail.com>
2023-10-08 18:03:19 +0200

    linearmodel tests added

---
+++ test_folder/LinearModel_test_suite.c
@@ -0,0 +1,31 @@
+#include "LinearModel_tests.h"
+
+static CU_TestInfo test_cases[] = {
+    { "test_init_linear", test_init_linear },
+    CU_TEST_INFO_NULL,
+};
+
+static CU_SuiteInfo suites[] = {
+    { "suite_name", NULL, NULL, NULL, NULL, test_cases },
+    CU_SUITE_INFO_NULL,
+};
+
+
+int main( void )
+{
+    if (CUE_SUCCESS != CU_initialize_registry())
+    {
+        return CU_get_error();
+    }
+
+    if (CUE_SUCCESS != CU_register_suites(suites)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return CU_get_error();
+}
