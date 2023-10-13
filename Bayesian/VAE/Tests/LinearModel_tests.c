a0ee0b337a784df5a17659e47d8f6241200f4da4
Linus Ryttermalm <linus.ryttermalm@prevas.se>
2023-10-13 17:12:11 +0200

    datatypes and lin tests

--- Bayesian/VAE/Tests/LinearModel_tests.c
+++ Bayesian/VAE/Tests/LinearModel_tests.c
@@ -4,7 +4,7 @@ void test_init_linear(void)
 {
 
 	LM *linear = InitLinear(4,4);
-	double expected = 1.0;
+	double expected = 0.1;
 	int r;
 	int c;
 	for(r = 0; r < linear->A->rows; r++)
@@ -22,6 +22,23 @@ void test_init_linear(void)
 			CU_ASSERT_EQUAL(linear->b->data[r][c], expected);
 		}
 	}
+	
 
 	freeLinear(linear);
+}
+
+void test_linear(void)
+{
+	LM *linear = InitLinear(4,4);
+	Matrix *m = allocateMatrix(1,4);
+
+	Linear(linear, m);
+
+	freeLinear(linear);
+	freeMatrix(m);
+}
+
+void test_backward(void)
+{
+
 }
